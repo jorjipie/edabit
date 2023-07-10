1efb687d5865867b6e65168c1cbdc03569f5f274
jorjipie <steve.farkas@gmail.com>
2023-07-09 22:51:43 -0400

    removing dup files

--- 2023_06_25_Modify_Words.js
+++
@@ -1,9 +0,0 @@
-// https://edabit.com/challenge/zwjWN6ehCZ3fAqvYo
-
-const editWords = arr => arr.map(w => 
-    (w.substring(0, w.length / 2) 
-        + '-' + w.substring(w.length / 2))
-        .toUpperCase().split('').reverse().join('')); 
-
-console.log(editWords(["new york city"]));
-console.log(editWords(["null", "undefined"]));
--- 2023_06_26_Average_Word_Length.js
+++
@@ -1,9 +0,0 @@
-// https://edabit.com/challenge/ACeSvyhYe7Qrtsk5R
-
-const averageWordLength = str => {
-    const arr = str.match(/[A-Za-z]+/g);
-    return Math.round(arr.reduce((a,c) => a + c.length, 0) / arr.length * 100) / 100;
-};
-
-console.log(averageWordLength("Dude, this is so awesome!"));
-console.log(averageWordLength('I just planted a young oak tree, wonder how tall it will grow in a few years?'));
