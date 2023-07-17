344abd374f98327b8ed8a1dc15b29ae75665fc7e
Steve <steve.farkas@gmail.com>
2023-07-16 21:47:17 -0400

    tidying up and refactoring

--- 2023_07/16_in_n_days.js
+++ 2023_07/16_in_n_days.js
@@ -2,21 +2,16 @@
     https://edabit.com/challenge/fLDHdMhmaNEd528wB
 */
 const weekdays = 
-       ['Sunday', 
-        'Monday', 
-        'Tuesday', 
-        'Wednesday', 
-        'Thursday', 
-        'Friday', 
-        'Saturday'];
+    ['Sunday', 
+    'Monday', 
+    'Tuesday', 
+    'Wednesday', 
+    'Thursday', 
+    'Friday', 
+    'Saturday'];
 
-const shiftDay = (day, shift) => {
-    let dayIndex = weekdays.indexOf(day);
-    let newIndex = (dayIndex + shift) % 7;
-    return weekdays[newIndex];
-};
 const afterNDays = (arr, shift) => 
-    arr.map(day => shiftDay(day, shift));
-    
+    arr.map(day => weekdays[(weekdays.indexOf(day) + shift) % 7]);
+
 console.log(afterNDays(["Saturday","Wednesday","Wednesday"], 10));
 //  ["Tuesday", "Saturday","Saturday"]
