65d8af7535f6167abc8507532ffb10856e958b21
jorjipie <steve.farkas@gmail.com>
2025-02-11 11:11:15 -0500

    added hall monitor challenge

---
+++ 2025/02/11_hall_monitor_2.js
@@ -0,0 +1,15 @@
+// https://edabit.com/challenge/yPa5YwAoqfdJuWK4C
+
+function possiblePath(arr) {
+	let layout = [1, 2, "H", 4, 3];
+    return arr.every((room, index) => {
+        if (index == 0) { return true; } 
+        return Math.abs(layout.indexOf(room) - layout.indexOf(arr[index - 1])) <= 1;
+    });
+
+}
+console.log(possiblePath([1, 2, "H", 4, 3]));
+console.log(possiblePath([1, 3, 4]) == false);
+console.log(possiblePath(["H", 2, 1, "H", 2]) == false);
+console.log(possiblePath([4, 3, 4, "H", 4, "H"]));
+
