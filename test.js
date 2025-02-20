01d77a23327ee68e29c066bf69c26dc18824404d
jorjipie <steve.farkas@gmail.com>
2025-02-20 15:40:50 -0500

    adding assertsimilar

--- test.js
+++ test.js
@@ -6,6 +6,15 @@ const assertEquals = (test, control, note) => {
         console.log(message);
     }
 };
+const assertSimilar = (test, control, note) => {
+    if (test.toString() == control.toString()) { console.log(true); }
+    else {
+        let message = `Failed. Expected ${control}. Got ${test}.`;
+        if (note != null) { message += " " + note; }
+        console.log(message);
+    }
+};
 module.exports = {
-    assertEquals
+    assertEquals, 
+    assertSimilar
 };
