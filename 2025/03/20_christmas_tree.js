76db01ef59a893f7a0362801d46df6d3e5983067
jorjipie <steve.farkas@gmail.com>
2025-03-20 10:23:39 -0400

    christmas tree challenge

--- 2025/03/20_christmas_tree.js
+++ 2025/03/20_christmas_tree.js
@@ -1,6 +1,16 @@
 // https://edabit.com/challenge/QeaCDhw3QrLwbeteD
 function tree(h) {
-
+    let arr = new Array(h)
+        , width = h * 2 - 1
+        , margin = 0;
+    for (let row = h - 1; row > -1; row--) {
+        arr[row] = ' '.repeat(margin) 
+            + '#'.repeat(width) 
+            + ' '.repeat(margin);
+        width -= 2;
+        margin++;
+    }
+    return arr;
 }
 
 const Test = require('../../test.js');
