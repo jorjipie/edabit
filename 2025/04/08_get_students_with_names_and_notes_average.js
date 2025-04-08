ebbb9c3130d8bc32710f53546f45c674e3261593
jorjipie <steve.farkas@gmail.com>
2025-04-08 12:06:22 -0400

    student names with notes arrow function challenge

---
+++ 2025/04/08_get_students_with_names_and_notes_average.js
@@ -0,0 +1,48 @@
+// https://edabit.com/challenge/aZiwEFEdFvWoBvDWS
+
+function getStudentsWithNamesAndAvgNote(students) {
+	return students.map(student => {
+        console.log(student);
+        return { name: student.name, avgNote: student.notes.length == 0 ? 0 : student.notes.reduce((sum, note) => sum + note) / student.notes.length };
+    });
+}
+
+const Test = require('../../test.js');
+const objectsAreEqual = (actual, expected) => Object.keys(expected).every(
+    key => actual[key] === expected[key]
+  )
+  const areEqual = (actual, expected, compare) => expected.every(
+    (item, index) => compare(actual[index], item)
+  )
+  const sets = [
+    [['Jacek', [5, 4, 3]], ['Ewa', [3, 3, 3]], ['Zygmunt', [1, 2, 3]]],
+    [['Jacek', []], ['Ewa', []], ['Zygmunt', [1, 2, 3]]],
+    []
+  ].map(
+    set => ({ 
+      actual: getStudentsWithNamesAndAvgNote(
+        set.map(([name, notes]) => ({ name, notes }))
+      ), 
+      expected: set.map(
+        ([name, notes]) => ({ 
+        name, 
+        avgNote: notes.reduce(
+         (total, next) => total + next, 
+         0
+        ) / (notes.length || 1) 
+      }))
+    })   
+  ).forEach(
+    ({ actual, expected }) => {
+      Test.expectNoError('Result should be an array', () => {
+        Test.assertEquals(
+          areEqual(
+            actual, 
+            expected, 
+            objectsAreEqual
+          ), 
+          true, 
+          `Actual: ${JSON.stringify(actual)}, Expected: ${JSON.stringify(expected)}`)
+      })
+    }
+  )
